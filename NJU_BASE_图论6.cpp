ff8b9e654cf1744ae82af27a5ea0c2fc36bb319e
departures <250858445@qq.com>
2018-08-23 11:14:30 +0800

    8.23

--- NJU_BASE_图论6.cpp
+++ NJU_BASE_图论6.cpp
@@ -1,10 +1,13 @@
 //输出每个标号的重量赋值情形
 //无解输出-1
-//李思勉的想法：1到n每个点依次确定他们的所有父节点，在安排节点时检查是否有父节点尚未进来，若有，则空出位置；（如果在讯在的过程中发现已经进栈的元素，说明存在环，return false）
+//失算了，应该用结构数组的。
+//LDawn的想法：1到n每个点依次确定他们的所有父节点，在安排节点时检查是否有父节点尚未进来，若有，则空出位置；（如果在讯在的过程中发现已经进栈的元素，说明存在环，return false）
 //就是贪心的问题嘛，快打快打！
 //方法2：先插进去，该位置有人，检查它的父节点是否为自己，若是，则占据该节点，使该节点之后的元素全部后移；若不是接着往下搜索，并重复此流程
 //方法三：拓扑
 //方法四：差分约束
+//爆炸式兴奋，方法一成功！
+
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -35,9 +38,22 @@ bool dfs(int u,int ans)
 	}
 	vis[u] = true;
 	s.pop(); 
-	if(ans!=-1)
-	fan[ans] += fan[u] + 1;
+	if (ans != -1)
+	{
+		int num = fan[ans];
+		for (int j = 0; fa[u][j] != -1; j++)
+			fa[ans][j] = fa[u][j];
+		fa[ans][num] = u;
+		fan[ans] += fan[u] + 1;
+	}
 	return true;
+}
+void init()
+{
+	memset(fa, -1, sizeof(fa));
+	memset(vis, false, sizeof(vis));
+	memset(fan, 0, sizeof(fan));
+	memset(ler, false, sizeof(ler));
 }
 int main()
 {
@@ -47,10 +63,7 @@ int main()
 		bool result=true;
 		int out[maxn] = {};
 		cin >> N >> M;
-		memset(vis, false, sizeof(vis));
-		memset(fan, 0, sizeof(fan));
-		memset(out, 0, sizeof(out));
-		memset(ler, false, sizeof(ler));
+		init();
 		for (int i = 0; i < M; i++)
 		{
 			int a, b;
@@ -74,7 +87,21 @@ int main()
 			int n = fan[i];
 			while (out[n])
 				n++;
-			out[n] = i;
+			int facount = 0;
+			int fanum = 0;
+			for (int j = 0; j < n; j++)
+			{
+				for (int k = 0; fa[i][k]!=-1; k++)
+				{
+					fanum++;
+					if (out[j] == fa[i][k])
+						facount++;
+				}
+			}
+			int sub = fanum - facount;
+			while (out[n + sub])
+				n++;
+			out[n+sub] = i;
 		}
 		if (!result)continue;
 		else
@@ -85,7 +112,6 @@ int main()
 			}
 			cout << endl;
 		}
-		
 	}
 	return 0;
 }
